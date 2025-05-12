8667758e77af2e0d5baec9bf8026b4a58335d66e
melvindecrypt <melvindecrypt@gmail.com>
2025-05-11 17:09:11 -0700

    Update wallets.js

--- Routes/wallets.js
+++ Routes/wallets.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const requireAuth = require('../middlewares/requireAuth');
+const { Authenticate }= require('../middlewares/AuthMiddleware);
 const requireVerifiedEmail = require('../middlewares/requireVerifiedEmail');
 const requireKYC = require('../middlewares/requireKYC');
 const walletController = require('../controllers/walletController'); // Assuming this file
 
 // Routes
 // Get the user's wallet details (View wallet)
-router.get('/', requireAuth, walletController.getWallet);
+router.get('/', Authenticate, walletController.getWallet);
 
 // Simulate deposit of funds
-router.post('/deposit', requireAuth, walletController.depositFunds);
+router.post('/deposit', Authenticate, walletController.depositFunds);
 
 // Withdraw funds (Requires KYC approval) - Create withdrawal request
-router.post('/withdraw', requireAuth, requireKYC, walletController.createWithdrawal);
+router.post('/withdraw', Authenticate, requireKYC, walletController.createWithdrawal);
 
 // Get withdrawal history
-router.get('/withdrawals', requireAuth, walletController.getWithdrawalHistory);
+router.get('/withdrawals', Authenticate, walletController.getWithdrawalHistory);
 
 module.exports = router;
