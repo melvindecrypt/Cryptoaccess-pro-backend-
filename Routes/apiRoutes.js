f405e6e96b462e6693b81aca60c0a958862db8a1
melvindecrypt <melvindecrypt@gmail.com>
2025-05-14 08:39:59 -0700

    Update apiRoutes.js

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -2,8 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController'); // Adjust path as needed
+const CurrencyController = require('../controllers/CurrencyController');
 const authMiddleware = require('../middleware/authMiddleware'); // Adjust path as needed
 
 router.get('/dashboard', authMiddleware, UserController.getDashboardData); // Pointing to your existing function
+
+// Example with authentication:
+router.get('/currencies', authMiddleware, CurrencyController.getAllCurrencies);
 
 module.exports = router;
