a4cdbadb89a1b2dbdf5760a9cbc36adbd6257385
melvindecrypt <melvindecrypt@gmail.com>
2025-05-14 19:50:19 -0700

    Update kyc.js

--- Routes/kyc.js
+++ Routes/kyc.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../config/fileStorage');
-const Authenticate = require('../middlewares/authMiddleware);
+const { kycUpload } = require('../config/fileStorage');
+const Authenticate = require('../middlewares/authMiddleware');
 const auditLog = require('../middlewares/auditLog');
 const kycController = require('../controllers/kycController');
 
-// Route to submit KYC documents
 router.post(
   '/submit',
   Authenticate,
-  upload.fields([
-    { name: 'idImage', maxCount: 1 },
-    { name: 'selfieImage', maxCount: 1 }
+  kycUpload.fields([
+    { name: 'idFront', maxCount: 1 },
+    { name: 'idBack', maxCount: 1 },
+    { name: 'selfie', maxCount: 1 } // Selfie is now always expected
   ]),
   auditLog('kyc_submission'),
   kycController.submitKYC
 );
- 
-// New route to get KYC status
+
 router.get('/status', Authenticate, kycController.getKYCStatus);
 
 module.exports = router;
