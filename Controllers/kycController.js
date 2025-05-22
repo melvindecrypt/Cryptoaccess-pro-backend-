c07a768c3506993e5fb67a913914f16100d4be79
melvindecrypt <melvindecrypt@gmail.com>
2025-05-22 06:05:35 -0700

    Update kycController.js

--- Controllers/kycController.js
+++ Controllers/kycController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/user');
 const { formatResponse } = require('../utils/helpers');
 const logger = require('../utils/logger');
 const AuditService = require('../services/auditService');
