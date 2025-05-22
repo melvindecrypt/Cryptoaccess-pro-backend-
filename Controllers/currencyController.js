1b88a74f9788f7cebdbf60284c9c72b19afe730b
melvindecrypt <melvindecrypt@gmail.com>
2025-05-22 13:11:13 +0100

    Update and rename CurrencyController.js to currencyController.js

--- Controllers/currencyController.js
+++ Controllers/currencyController.js
@@ -1,4 +1,4 @@
-const Currency = require('../models/Currency');
+const currency = require('../models/Currency');
 const { formatResponse } = require('../utils/helpers'); // Adjust the path if necessary
 
 exports.getAllCurrencies = async (req, res) => {
