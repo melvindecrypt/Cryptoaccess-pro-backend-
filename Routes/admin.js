13b1c5a30dc6d5866835a4969380b4661573cd1f
melvindecrypt <melvindecrypt@gmail.com>
2025-05-22 07:40:21 -0700

    Update admin.js

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const rateLimit = require('express-rate-limit');
-const User = require('../models/User');
+const User = require('../models/user');
 const { authenticate, isAdmin } = require('../middlewares/authMiddleware');
 const logger = require('../utils/logger');
 const { formatResponse } = require('../utils/helpers');
