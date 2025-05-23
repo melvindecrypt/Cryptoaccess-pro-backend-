695c92b90dd7bfc8b7e6655f772599e263a3ff8b
melvindecrypt <melvindecrypt@gmail.com>
2025-05-23 11:01:57 -0700

    Update wallets.js

--- Routes/wallets.js
+++ Routes/wallets.js
@@ -1,37 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const { Authenticate }= require('../middlewares/AuthMiddleware);
+const { authenticate }= require('../middlewares/AuthMiddleware);
 const requireVerifiedEmail = require('../middlewares/requireVerifiedEmail');
 const requireKYC = require('../middlewares/requireKYC');
 const walletController = require('../controllers/walletController'); // Assuming this file
+const { getDepositAddress } = require('../controllers/walletController');
 
 // Routes
 // Get the user's wallet details (View wallet)
-router.get('/', Authenticate, walletController.getWallet);
+router.get('/', authenticate, walletController.getWallet);
 
 // Simulate deposit of funds
-router.post('/deposit', Authenticate, walletController.depositFunds);
+router.post('/deposit', authenticate, walletController.depositFunds);
 
 // Withdraw funds (Requires KYC approval) - Create withdrawal request
-router.post('/withdraw', Authenticate, requireKYC, walletController.createWithdrawal);
+router.post('/withdraw', authenticate, requireKYC, walletController.createWithdrawal);
 
 // Get withdrawal history
-router.get('/withdrawals', Authenticate, walletController.getWithdrawalHistory);
-
-module.exports = router;
-
-// In routes/wallets.js
-const express = require('express');
-const router = express.Router();
-const { authenticate } = require('../middleware/authMiddleware');
-const { getDepositAddress, /* other wallet functions */ } = require('../controllers/walletController');
+router.get('/withdrawals', authenticate, walletController.getWithdrawalHistory);
 
 router.get('/deposit-address', authenticate, getDepositAddress);
 // Add other wallet routes here
 
-module.exports = router;
 // In routes/wallets.js
-router.get('/balances', Authenticate, walletController.getUserBalances);
+router.get('/balances', authenticate, walletController.getUserBalances);
 
-
+module.exports = router;
 
