4d1422c6f3576178f5c013ae8ea1d7ee6dfeea05
melvindecrypt <melvindecrypt@gmail.com>
2025-05-22 20:58:35 -0700

    Update investments.js

--- Routes/investments.js
+++ Routes/investments.js
@@ -1,7 +1,7 @@
 // routes/investments.js
 const express = require('express');
 const router = express.Router();
-const { Authenticate } = require('../middlewares/authMiddleware);
+const { authenticate } = require('../middlewares/authMiddleware);
 const requireVerifiedEmail = require('../middlewares/requireVerifiedEmail');
 
 // Controllers (assume these are implemented in controllers/investmentController.js)
@@ -9,23 +9,17 @@ const { viewPlans, invest, trackInvestment } = require('../controllers/investmen
 
 // Routes
 // View available investment plans
-router.get('/plans', Authenticate, viewPlans);
+router.get('/plans', authenticate, viewPlans);
 
 // Start a new investment (does not require KYC approval)
-router.post('/invest', Authenticate, invest);
+router.post('/invest', authenticate, invest);
 
 // Track user's current investments
 router.get('/my-investments', authenticate, trackInvestment);
 
-module.exports = router;
-
-
-const express = require('express');
-const router = express.Router();
 const investmentController = require('../controllers/investmentController');
-const { Authenticate } = require('../middleware/authMiddleware');
 
 router.get('/:id', auth, investmentController.getInvestmentDetails);
-router.post('/:id/cancel', Authenticate, investmentController.cancelInvestment);
+router.post('/:id/cancel', authenticate, investmentController.cancelInvestment);
 
 module.exports = router;
