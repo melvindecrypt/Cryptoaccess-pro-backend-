8dbeefb394bbb1f202e709b3409c4c8519987b62
melvindecrypt <melvindecrypt@gmail.com>
2025-05-23 12:52:28 -0700

    Update currency.js

--- Routes/currency.js
+++ Routes/currency.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const currencyController = require('../controllers/currencyController');
-const authMiddleware = require('../middleware/authMiddleware');
+const { authenticate } = require('../middleware/authMiddleware');
 
 // Example with authentication:
-router.get('/currencies', authMiddleware, currencyController.getAllCurrencies);
+router.get('/currencies', authenticate, currencyController.getAllCurrencies);
 
 module.exports = router;
