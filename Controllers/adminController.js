516edfe50e6015b9fb54b95bfbdbfa62b0dcb7d7
melvindecrypt <melvindecrypt@gmail.com>
2025-05-22 21:49:26 -0700

    Update adminController.js

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -25,7 +25,7 @@ const notificationService = require('../services/notificationService');
 
     // Security checks
     if (!user?.isAdmin) {
-      logger.warn('Admin login attempt failed: Icorrect email or password', { email });
+      logger.warn('Admin login attempt failed: Incorrect email or password', { email });
       return res.status(403).json(formatResponse(false, 'Access denied'));
     }
 
