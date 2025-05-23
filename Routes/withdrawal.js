ae667552f227cdd6c462139c7bca1adcca240acb
melvindecrypt <melvindecrypt@gmail.com>
2025-05-23 11:02:28 -0700

    Update withdrawal.js

--- Routes/withdrawal.js
+++ Routes/withdrawal.js
@@ -1,10 +1,10 @@
 // File: routes/withdrawal.js
 const express = require('express');
 const router = express.Router();
-const { Authenticate } = require('../middleware/authMiddleware');
+const { authenticate } = require('../middleware/authMiddleware');
 const withdrawalController = require('../controllers/withdrawalController');
 
 // Withdraw funds (Requires Pro+ and KYC approval)
-router.post('/', Authenticate, withdrawalController.createWithdrawal);
+router.post('/', authenticate, withdrawalController.createWithdrawal);
 
 module.exports = router;
