c8e5cd31a2f396ff0de98453b4609c53fa6baf7a
melvindecrypt <melvindecrypt@gmail.com>
2025-05-26 19:28:44 -0700

    Update investmentPlan.js

--- Models/investmentPlan.js
+++ Models/investmentPlan.js
@@ -1,16 +1,18 @@
-// models/InvestmentPlan.js
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
-const investmentPlanSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  minAmount: { type: Number, required: true },
-  roi: { type: Number, required: true },
-  duration: { type: String, required: true },
-  status: { type: String, default: 'available' },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-});
+const investmentPlanSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    minAmount: { type: Number, required: true },
+    roi: { type: Number, required: true },
+    duration: { type: String, required: true },
+    status: { type: String, default: 'available' },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true } // Automatically manages `createdAt` and `updatedAt`
+);
 
 const InvestmentPlan = mongoose.model('InvestmentPlan', investmentPlanSchema);
 
-module.exports = InvestmentPlan;
+export default InvestmentPlan;
