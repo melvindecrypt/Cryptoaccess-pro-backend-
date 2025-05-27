84cc132f623ccfdf8c3c374feb4fc0361e9f0c3e
melvindecrypt <melvindecrypt@gmail.com>
2025-05-26 20:33:30 -0700

    Update SubscriptionValidators.js

--- validators/SubscriptionValidators.js
+++ validators/SubscriptionValidators.js
@@ -1,25 +1,26 @@
-// validators/subscriptionValidators.js
-const Joi = require('joi');
+import Joi from 'joi';
 
 const validateProPlusPayment = (paymentData) => {
   const schema = Joi.object({
-    userId: Joi.string().required().messages({
-      'string.empty': 'User ID is required.',
-      'any.required': 'User ID is required.'
-    }),
-    transactionId: Joi.string().required().messages({
-      'string.empty': 'Transaction ID is required.',
-      'any.required': 'Transaction ID is required.'
-    }),
-    // You might want to add validation for other fields that could be in the
-    // payment confirmation request, such as:
-    // paymentMethod: Joi.string().optional(),
-    // paymentDate: Joi.date().optional(),
-    // amountPaid: Joi.number().positive().optional(),
+    userId: Joi.string()
+      .required()
+      .messages({
+        'string.empty': 'User ID is required.',
+        'any.required': 'User ID is required.',
+      }),
+    transactionId: Joi.string()
+      .required()
+      .messages({
+        'string.empty': 'Transaction ID is required.',
+        'any.required': 'Transaction ID is required.',
+      }),
+    // Optional fields for additional payment details
+    paymentMethod: Joi.string().optional(),
+    paymentDate: Joi.date().optional(),
+    amountPaid: Joi.number().positive().optional(),
   });
+
   return schema.validate(paymentData);
 };
 
-module.exports = {
-  validateProPlusPayment,
-};
+export { validateProPlusPayment };
