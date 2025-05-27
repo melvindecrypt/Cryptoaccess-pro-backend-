acacb8671ee80a3dad377c9020147f28ab84bd7c
melvindecrypt <melvindecrypt@gmail.com>
2025-05-26 19:31:24 -0700

    Update settings.js

--- Models/settings.js
+++ Models/settings.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
-const settingsSchema = new mongoose.Schema({
-  accessFee: { type: Number, default: 99.9 },
-  proPlusFee: { type: Number, default: 299.99 },
-  investmentMin: { type: Number, default: 500 },
-  investmentMax: { type: Number, default: 100000 },
-  withdrawalsEnabled: { type: Boolean, default: true }
-});
+const settingsSchema = new mongoose.Schema(
+  {
+    accessFee: { type: Number, default: 99.9 },
+    proPlusFee: { type: Number, default: 299.99 },
+    investmentMin: { type: Number, default: 500 },
+    investmentMax: { type: Number, default: 100000 },
+    withdrawalsEnabled: { type: Boolean, default: true },
+  },
+  { timestamps: true } // Optionally add timestamps for tracking changes
+);
 
-module.exports = mongoose.model('Settings', settingsSchema);
+export default mongoose.model('Settings', settingsSchema);
