90b6a9ec29ed46b2bca31b010ea734a56c276516
melvindecrypt <melvindecrypt@gmail.com>
2025-05-26 19:26:21 -0700

    Update currency.js

--- Models/currency.js
+++ Models/currency.js
@@ -1,29 +1,32 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
-const CurrencySchema = new mongoose.Schema({
-  symbol: {
-    type: String,
-    required: true,
-    unique: true,
-    uppercase: true,
-    trim: true,
+const CurrencySchema = new mongoose.Schema(
+  {
+    symbol: {
+      type: String,
+      required: true,
+      unique: true,
+      uppercase: true,
+      trim: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    logoUrl: {
+      type: String,
+      trim: true,
+    },
+    // Add other relevant fields as needed in the future
   },
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  isActive: {
-    type: Boolean,
-    default: true,
-  },
-  logoUrl: {
-    type: String,
-    trim: true,
-  },
-  // Add other relevant fields as needed in the future
-});
+  { timestamps: true } // Optionally add timestamps for createdAt and updatedAt
+);
 
 const Currency = mongoose.model('Currency', CurrencySchema);
 
-module.exports = Currency;
+export default Currency;
