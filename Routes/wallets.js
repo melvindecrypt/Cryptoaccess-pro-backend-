eb2b86bccea2263f45555bbe97c9bb29e74b150c
melvindecrypt <melvindecrypt@gmail.com>
2025-05-28 18:40:42 -0700

    Update wallets.js

--- Routes/wallets.js
+++ Routes/wallets.js
@@ -14,12 +14,6 @@ router.get('/', authenticate, walletController.getWallet);
 // Simulate deposit of funds
 router.post('/deposit', authenticate, walletController.depositFunds);
 
-// Withdraw funds (Requires KYC approval) - Create withdrawal request
-router.post('/withdraw', authenticate, requireKYC, walletController.createWithdrawal);
-
-// Get withdrawal history
-router.get('/withdrawals', authenticate, walletController.getWithdrawalHistory);
-
 // Get deposit address
 router.get('/deposit-address', authenticate, walletController.getDepositAddress);
 
@@ -29,9 +23,5 @@ router.get('/balances', authenticate, walletController.getUserBalances);
 // Route for sending funds internally to another user on the platform
 // POST /api/wallet/send-internal
 router.post('/send-internal', authenticate, requireKYC, walletController.sendInternalFunds);
-
-// Route for requesting external withdrawal
-// POST /api/wallet/request-withdrawal
-router.post('/request-withdrawal', authenticate, requireKYC, walletController.requestWithdrawal);
 
 export default router;
