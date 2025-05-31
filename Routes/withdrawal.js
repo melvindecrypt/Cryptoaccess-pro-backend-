3ff686d820fc98ebcae803a8fadce86d33384d2c
melvindecrypt <melvindecrypt@gmail.com>
2025-05-31 07:05:53 +0100

    Update withdrawal.js

--- Routes/withdrawal.js
+++ Routes/withdrawal.js
@@ -7,5 +7,7 @@ const router = express.Router();
 // Withdraw funds (Requires KYC approval) - Create withdrawal request
 router.post('/withdraw', authenticate, requireKYC, withdrawalController.initiateWithdrawal);
 
+// Get user balances
+router.get('/balances', authenticate, withdrawalController.getUserBalances);
 
 export default router;
