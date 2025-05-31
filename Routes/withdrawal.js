119d1c8ab9fe2c0eb8bb213b6905699b682bf70b
melvindecrypt <melvindecrypt@gmail.com>
2025-05-31 07:12:31 +0100

    Update withdrawal.js

--- Routes/withdrawal.js
+++ Routes/withdrawal.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import { authenticate } from '../middleware/authMiddleware.js';
+import requireVerifiedEmail from '../middlewares/requireVerifiedEmail.js';
+import { requireKYC } from '../middlewares/requireKYC.js';
 import withdrawalController from '../controllers/withdrawalController.js';
 
 const router = express.Router();
 
 // Withdraw funds (Requires KYC approval) - Create withdrawal request
-router.post('/withdraw', authenticate, requireKYC, withdrawalController.initiateWithdrawal);
+router.post('/withdraw', authenticate, requireKYC, requireVerifiedEmail, withdrawalController.initiateWithdrawal);
 
 // Get user balances
-router.get('/balances', authenticate, withdrawalController.getUserBalances);
+router.get('/balances', authenticate, requireKYC, requireVerifiedEmail, withdrawalController.getUserBalances);
 
 export default router;
